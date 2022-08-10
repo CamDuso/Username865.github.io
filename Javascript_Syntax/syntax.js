6f21f60d827cdf8c3e96204d4f1d7118cfb01df7
Username865 <55616274+Username865@users.noreply.github.com>
2022-08-09 21:01:12 -0400

    Add files via upload

---
+++ Javascript_Syntax/syntax.js
@@ -0,0 +1,48 @@
+let variableOne = 2
+let variableTwo = 4
+let variableThree = variableOne + variableTwo
+
+console.log (variableOne+variableTwo+variableThree)
+
+let variableFour = 6
+let variableFive = 8
+
+console.log (variableFive+variableFour-variableThree-variableTwo+variableOne)
+
+let A = 1
+let B = 2
+let C = 3
+let D = 4
+let E = 5
+let F = 6
+let G = 7
+let H = 8
+let I = 9
+let J = 10
+
+let average1 = ((A+B+C+D+E)/5)
+let average2 = ((F+G+H+I+J)/5)
+
+let sum1= ((average1+average2)/2)
+
+console.log (average1)
+console.log (average2)
+console.log (sum1)
+
+let value1 = 4
+let value2 = 10
+let value3 = 22
+let value4 = -30
+let value5 = 55
+let value6 = 68
+let value7 = 7
+let value8 = -22
+let value9 = 9
+let value10 = 100
+
+let average3 = ((value1+value2+value3+value4+value5)/5)
+let average4 = ((value6+value7+value8+value9+value10)/5)
+
+console.log (average3)
+console.log (average4)
+console.log ((average3+average4)/4)
