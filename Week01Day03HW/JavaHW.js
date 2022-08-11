9c393014983b3213f1064d90637811afe86f91e0
Username865 <55616274+Username865@users.noreply.github.com>
2022-08-11 08:58:57 -0400

    Add files via upload

---
+++ Week01Day03HW/JavaHW.js
@@ -0,0 +1,82 @@
+//For Loop practice
+
+// for(let i = 0; i < 10; i++){
+//     console.log (i)
+// }
+
+//Exercise 1
+
+// for(let i = 10; i > 0; i--){
+//     console.log (i)
+// }
+
+//Exercise 2
+
+// for (let i=1; i<=10; i+=2){
+//     console.log (i)
+//     if(i>8){
+//         for (let i =2;i<11;i+=2){
+//     }
+// }
+// }
+// for (let i=2; i<=10; i+=2){
+//     console.log(i)
+// }
+
+//Exercise 3
+
+// for (let i=6; i<=60; i++){
+//     if(i%3===0){
+//         console.log(i)
+//     }
+// }
+
+//Exercise 4
+
+// for (let i=1; i<=70000000; i*=10){
+//     console.log(i)
+// }
+
+// let number = "#"
+// for (let i=0; i<7; i++){
+//     console.log (number)
+//         number +="#"
+// }
+
+//Lab Practice
+
+// for (let i=0; i <=10; i++){
+//     if(i%2===0)
+//     console.log(i)
+// }
+
+// let num = 54
+
+// if (num >= 80){
+//     console.log("Good Job")}
+//     else if (num >=70){
+//         console.log ("Keep Trying")
+//     }
+//     else if (num >=60){
+//             console.log ("Ehh")
+//         }
+//         else if (num >=55){
+//                 console.log ("Not Too Good")
+//             }
+//             else if (num >=0){ 
+//                 console.log ("You'll get em Next Time")
+
+
+//Fizzbuzz
+// for (let i=1; i<=100; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log ("Fizzbuzz")
+//     }
+//         else if(i%3===0){
+//             console.log ("Fizz")
+//         }
+//             else if(i%5===0){
+//                 console.log ("Buzz")
+//             }
+//             else {console.log (i)}
+//     }
