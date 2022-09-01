7c1bbb196c2b6f550c62eac6c713c6659400069c
Cam <camduso8@gmail.com>
2022-09-01 13:53:11 -0400

    RE re upload

--- Week04_Day03_Lab/Array_Object_Assignment/app.js
+++ Week04_Day03_Lab/Array_Object_Assignment/app.js
@@ -1,45 +1,53 @@
-
-
 const radio = {
-    stations: [station1 = {
-        name: "WKAF Hip-Hop",
-        songs: [{
-            title:"One Dance",
-            artist: "Drake"
-        },{
-            title:"Good News",
-            artist: "Mac Miller"
-        },{
-            title:"Gin and Juice",
-            artist: "Snoop"
-        },{
-            title:"Best Friend",
-            artist: "Fiddy"
-        }
-        ]
-    },
-    station2 = {
-        name: "FACE MELTER 107",
-        songs: [{
-            title:"Battery",
-            artist: "Metallica"
-        },{
-            title:"Madhouse",
-            artist: "Anthrax"
-        },{
-            title:"Sweating Bullets",
-            artist: "Megadeth"
-        },{
-            title:"Raining Blood",
-            artist: "Slayer"
-        }]
-    } 
-    ], changeStation (){
-        station = Math.floor(Math.random()*2)
-        song = Math.floor(Math.random()*4)
-        selection = radio.stations[station].songs[song]
-        console.log("Now Playing " + selection.title + ' by ' + selection.artist)
-    }
- }
-radio.changeStation()
-
+  stations: [
+    (station1 = {
+      name: "WKAF Hip-Hop",
+      songs: [
+        {
+          title: "One Dance",
+          artist: "Drake",
+        },
+        {
+          title: "Good News",
+          artist: "Mac Miller",
+        },
+        {
+          title: "Gin and Juice",
+          artist: "Snoop",
+        },
+        {
+          title: "Best Friend",
+          artist: "Fiddy",
+        },
+      ],
+    }),
+    (station2 = {
+      name: "FACE MELTER 107",
+      songs: [
+        {
+          title: "Battery",
+          artist: "Metallica",
+        },
+        {
+          title: "Madhouse",
+          artist: "Anthrax",
+        },
+        {
+          title: "Sweating Bullets",
+          artist: "Megadeth",
+        },
+        {
+          title: "Raining Blood",
+          artist: "Slayer",
+        },
+      ],
+    }),
+  ],
+  changeStation() {
+    station = Math.floor(Math.random() * 2);
+    song = Math.floor(Math.random() * 4);
+    selection = radio.stations[station].songs[song];
+    console.log("Now Playing " + selection.title + " by " + selection.artist);
+  },
+};
+radio.changeStation();
